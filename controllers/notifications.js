2e00e0526fecabc7c2028e4d8fe391d265e88fef
SAWADOGO Rimsomgdo Roger <rimssawadogo@gmail.com>
2024-12-05 21:32:35 +0100

    good

--- controllers/notifications.js
+++ controllers/notifications.js
@@ -81,25 +81,29 @@ const fetchAndNotify = async () => {
 
       if (isNaN(temperature) || isNaN(humidity) || isNaN(moisture) || isNaN(npk)) {
         console.log('Données invalides pour l\'utilisateur', user._id);
-        
+        continue;
       }
 
       console.log(`Température: ${temperature}°C, Humidité: ${humidity}%, Humidité du sol: ${moisture}%, NPK: ${npk}`);
 
-      let shouldSendNotification = false;
+      // Variables pour vérifier si une notification doit être envoyée
+      let shouldSendTemperatureNotification = false;
+      let shouldSendHumidityNotification = false;
+      let shouldSendMoistureNotification = false;
+      let shouldSendNPKNotification = false;
 
       // Vérifications des seuils et gestion des alertes pour chaque donnée
 
       // Température
       if (temperature < TEMPERATURE_MIN_THRESHOLD) {
         if (!user.alerts.temperatureLow) {
-          shouldSendNotification = true;
+          shouldSendTemperatureNotification = true;
           user.alerts.temperatureLow = true;
           user.alerts.temperatureHigh = false;
         }
       } else if (temperature > TEMPERATURE_MAX_THRESHOLD) {
         if (!user.alerts.temperatureHigh) {
-          shouldSendNotification = true;
+          shouldSendTemperatureNotification = true;
           user.alerts.temperatureHigh = true;
           user.alerts.temperatureLow = false;
         }
@@ -113,13 +117,13 @@ const fetchAndNotify = async () => {
       // Humidité
       if (humidity < HUMIDITY_MIN_THRESHOLD) {
         if (!user.alerts.humidityLow) {
-          shouldSendNotification = true;
+          shouldSendHumidityNotification = true;
           user.alerts.humidityLow = true;
           user.alerts.humidityHigh = false;
         }
       } else if (humidity > HUMIDITY_MAX_THRESHOLD) {
         if (!user.alerts.humidityHigh) {
-          shouldSendNotification = true;
+          shouldSendHumidityNotification = true;
           user.alerts.humidityHigh = true;
           user.alerts.humidityLow = false;
         }
@@ -133,13 +137,13 @@ const fetchAndNotify = async () => {
       // Humidité du sol
       if (moisture < MOISTURE_MIN_THRESHOLD) {
         if (!user.alerts.moistureLow) {
-          shouldSendNotification = true;
+          shouldSendMoistureNotification = true;
           user.alerts.moistureLow = true;
           user.alerts.moistureHigh = false;
         }
       } else if (moisture > MOISTURE_MAX_THRESHOLD) {
         if (!user.alerts.moistureHigh) {
-          shouldSendNotification = true;
+          shouldSendMoistureNotification = true;
           user.alerts.moistureHigh = true;
           user.alerts.moistureLow = false;
         }
@@ -153,13 +157,13 @@ const fetchAndNotify = async () => {
       // NPK
       if (npk < NPK_MIN_THRESHOLD) {
         if (!user.alerts.npkLow) {
-          shouldSendNotification = true;
+          shouldSendNPKNotification = true;
           user.alerts.npkLow = true;
           user.alerts.npkHigh = false;
         }
       } else if (npk > NPK_MAX_THRESHOLD) {
         if (!user.alerts.npkHigh) {
-          shouldSendNotification = true;
+          shouldSendNPKNotification = true;
           user.alerts.npkHigh = true;
           user.alerts.npkLow = false;
         }
@@ -173,55 +177,21 @@ const fetchAndNotify = async () => {
       // Sauvegarde des modifications dans la base de données
       await user.save();
 
-      // Envoi de la notification si nécessaire
-      if (shouldSendNotification) {
-        let alertType;
+      // Envoi des notifications indépendantes si nécessaire
+      if (shouldSendTemperatureNotification) {
+        await sendNotification(Token, 'Température critique', `La température est ${temperature}°C.`);
+      }
 
-        // Notifications en fonction des seuils pour chaque donnée
-        if (temperature < TEMPERATURE_MIN_THRESHOLD) {
-          alertType = {
-            title: 'Température basse',
-            body: `La température est tombée à ${temperature}°C.`,
-          };
-        } else if (temperature > TEMPERATURE_MAX_THRESHOLD) {
-          alertType = {
-            title: 'Température élevée',
-            body: `La température a atteint ${temperature}°C.`,
-          };
-        } else if (humidity < HUMIDITY_MIN_THRESHOLD) {
-          alertType = {
-            title: 'Humidité air basse',
-            body: `L'humidité air est tombée à ${humidity}%.`,
-          };
-        } else if (humidity > HUMIDITY_MAX_THRESHOLD) {
-          alertType = {
-            title: 'Humidité air élevée',
-            body: `L'humidité air a atteint ${humidity}%.`,
-          };
-        } else if (moisture < MOISTURE_MIN_THRESHOLD) {
-          alertType = {
-            title: 'Humidité du sol basse',
-            body: `L'humidité du sol est tombée à ${moisture}%.`,
-          };
-        } else if (moisture > MOISTURE_MAX_THRESHOLD) {
-          alertType = {
-            title: 'Humidité du sol élevée',
-            body: `L'humidité du sol a atteint ${moisture}%.`,
-          };
-        } else if (npk < NPK_MIN_THRESHOLD) {
-          alertType = {
-            title: 'NPK faible',
-            body: `Les niveaux de NPK sont tombés à ${npk}.`,
-          };
-        } else if (npk > NPK_MAX_THRESHOLD) {
-          alertType = {
-            title: 'NPK élevé',
-            body: `Les niveaux de NPK ont atteint ${npk}.`,
-          };
-        }
+      if (shouldSendHumidityNotification) {
+        await sendNotification(Token, 'Humidité critique', `L'humidité est ${humidity}%.`);
+      }
 
-        // Envoi de la notification
-        await sendNotification(Token, alertType.title, alertType.body);
+      if (shouldSendMoistureNotification) {
+        await sendNotification(Token, 'Humidité du sol critique', `L'humidité du sol est ${moisture}%.`);
+      }
+
+      if (shouldSendNPKNotification) {
+        await sendNotification(Token, 'Niveau NPK critique', `Les niveaux de NPK sont ${npk}.`);
       }
     }
   } catch (error) {
@@ -257,10 +227,32 @@ notifs.post('/fetchData', async (req, res) => {
         params: { api_key: thingSpeakApiKey, results },
       }
     );
-    res.status(200).json(response.data);
+
+    const feeds = response.data.feeds;
+    if (feeds.length === 0) {
+      return res.status(404).json({ message: 'Aucune donnée disponible.' });
+    }
+
+    const latestData = feeds[results - 1];
+    const temperature = parseFloat(latestData.field1);
+    const humidity = parseFloat(latestData.field2);
+    const moisture = parseFloat(latestData.field3);
+    const npk = parseFloat(latestData.field4);
+
+    if (isNaN(temperature) || isNaN(humidity) || isNaN(moisture) || isNaN(npk)) {
+      return res.status(400).json({ message: 'Données invalides.' });
+    }
+
+    // Retourner les données dans la réponse
+    res.json({
+      temperature,
+      humidity,
+      moisture,
+      npk,
+    });
   } catch (error) {
-    console.error('Erreur lors de la récupération des données :', error.message);
-    res.status(500).json({ message: 'Erreur lors de la récupération des données.' });
+    console.error('Erreur lors de la récupération des données:', error.message);
+    res.status(500).json({ message: 'Erreur interne du serveur.' });
   }
 });
 
